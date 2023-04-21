48c3cfb847178892c9c7c2e77cde983f1bb7652e
ritakool <jokernguyen1994@gmail.com>
2023-04-22 02:38:19 +0700

    complete mistake and total penalty

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,8 @@ public class Main {
 //        Staff staff1 = new FullTimeStaff("#1","Nguyễn Văn Tèo", LocalDate.parse("1994-07-09"),"(+84)-(123456789)",new Address("Hà Nội","Ba Đình","34","425"));
 //        Staff staff2 = new PartTimeStaff("#2","Mộng Hà", LocalDate.parse("2005-07-09"),"(+84)-(123456789)",new Address("Hà Nội","Ba Đình","34","425"));
 //        Staff staff3 = new FullTimeStaff("#3","Nguyễn Văn Hà", LocalDate.parse("2000-07-31"),"(+84)-(123456789)",new Address("Hà Nội","Ba Đình","34","425"));
-//        Staff staff4 = new FullTimeStaff("#5","Nguyễn Văn Hà", LocalDate.parse("2000-08-31"),"(+84)-(123456709)",new Address("Hà Nội","Ba Đình","34","425"));
-//        managerStaff.addStaff(staff4);
+        Staff staff4 = new FullTimeStaff("#5","Nguyễn Văn Hà", LocalDate.parse("2000-08-31"),"(+84)-(123456709)",new Address("Hà Nội","Ba Đình","34","425"));
+        managerStaff.addStaff(staff4);
 //        managerStaff.addStaff(staff1);
 //        managerStaff.addStaff(staff3);
 //        managerStaff.addStaff(staff2);
@@ -29,29 +29,6 @@ public class Main {
 //        System.out.println("sắp xếp");
 //        managerStaff.arrangement();
 //        managerStaff.removeStaff("#2");
-//        Employee employee = new Employee("hà",19,1994,"mama");
-//        System.out.println(employee.toString(true));
-//        System.out.println(employee);
 
-        Scanner scanner = new Scanner(System.in);
-        FullTimeStaff fullTimeStaff = new FullTimeStaff();
-        // Nhập thông tin của nhân viên
-        System.out.print("Nhập tên nhân viên: ");
-        String name = scanner.nextLine();
-        System.out.print("Nhập số ngày đi muộn: ");
-        int lateDays = scanner.nextInt();
-
-        // Tính tiền thưởng/phạt
-        int totalRewardPunishment = 0;
-        if (lateDays > 0) {
-            int amount = FullTimeStaff.RewardPunishment.getAmountByName("LATE_ATTENDANCE");
-            totalRewardPunishment -= lateDays * amount;
-            System.out.println("Phạt " + (-totalRewardPunishment) + " đồng vì đi muộn " + lateDays + " ngày");
-        } else {
-            System.out.println("Không bị phạt vì không đi muộn");
-        }
-
-        // In tổng tiền thưởng/phạt của nhân viên
-        System.out.println("Tổng tiền thưởng/phạt của nhân viên " + name + " là " + totalRewardPunishment + " đồng");
     }
 }
--- src/model/FullTimeStaff.java
+++ src/model/FullTimeStaff.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 public class FullTimeStaff extends Staff{
     private final double BASIC_SALARY = 7000;
-    private int numberOfMistake;
     private String nameMistake;
     private enum mistake {
         FORGOT_REPORT("Quên làm báo cáo tuần", 150),
@@ -16,73 +15,59 @@ public class FullTimeStaff extends Staff{
             this.name = name;
             this.amount = amount;
         }
-        public static double getAmountByName(String name) {
-            for (mistake rp : mistake.values()) {
-                if (rp.name.equals(name)) {
-                    return rp.amount;
-                }
-            }
-            return 0;
+        public String getName() {
+            return name;
+        }
+        public double getAmountMistake() {
+            return amount;
         }
     }
-    private enum reward {
-        GOOD_QUALITY("Hoàn thành tốt nhiệm vụ được giao", 0.1),
-        CONTRIBUTE_TO_TEAM("Đóng góp tích cực trong nhóm", 0.05),
-        SUGGEST_SOLUTION("Ý tưởng mới", 0.03);
-    }
-    private int[] errorCount = new int[mistake.values().length];
 
     public FullTimeStaff() {
     }
+
     public FullTimeStaff(String id, String name, LocalDate birthDay, String tel, Address address) {
         super(id, name, birthDay, tel, address);
     }
-
-    public double bonus() {
-        double bonus = 0;
-        return bonus;
+    private int[] errorCount = new int[mistake.values().length];
+    public void addMistake(mistake mistake) {
+        errorCount[mistake.ordinal()]++;
     }
-    pu
     public double penalty() {
-        double penalty;
-        if (numberOfMistake > 0) {
-            double amount = mistake.getAmountByName(nameMistake);
-            penalty = amount * numberOfMistake;
-        } else {
-            penalty = 0;
+        double penalty = 0;
+        for (int i = 0; i < errorCount.length; i++ ) {
+            double amount = mistake.values()[i].getAmountMistake();
+            penalty = amount * errorCount[i];
         }
         return penalty;
     }
-
-    public int getNumberOfMistake() {
-        return numberOfMistake;
+    public String getMistakes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Danh sách lỗi: \n");
+        for (int i = 0; i < errorCount.length; i++ ) {
+            if (errorCount[i] > 0) {
+                sb.append(mistake.values()[i].getName())
+                        .append(": ")
+                        .append(errorCount[i])
+                        .append("\n");
+            }
+        }
+        return sb.toString();
     }
 
-    public void setNumberOfMistake(int numberOfMistake) {
-        this.numberOfMistake = numberOfMistake;
-    }
-
-    public String getNameMistake() {
-        return nameMistake;
-    }
-
-    public void setNameMistake(String nameMistake) {
-        this.nameMistake = nameMistake;
-    }
 
     @Override
     public double payroll() {
-        return BASIC_SALARY + bonus() - penalty();
+        return BASIC_SALARY  - penalty();
     }
-
     @Override
     public String toString() {
         return  super.toString() +
                 "Nhân viên chính thức: " + '\n' +
                 "Lương cơ bản = " + BASIC_SALARY + '\t' +
-                "Thưởng: " + bonus() +'\t' +
+                "Thưởng: " + '\t' +
                 "Phạt: " + penalty() + '\t' +
-                "Tổng lương: " + payroll() + "\n" +
+                "Tổng lương: " + payroll() + "\t" + getMistakes() + '\n' +
                 "---------------";
     }
 }
